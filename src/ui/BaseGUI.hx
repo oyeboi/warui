ff9d4aef13a854435a512659a8d70f3184c3f6e6
iamnottony <pharrowant@gmail.com>
2024-09-19 03:25:46 -0400

    UPDATE BaseGUI with Window object functions

--- src/ui/BaseGUI.hx
+++ src/ui/BaseGUI.hx
@@ -16,11 +16,15 @@ class BaseGUI {
     static var hideCursor: Bool;
     static var cursorCache: haxe.ds.StringMap<hxd.Cursor.CustomCursor>;
     static var curNativeCursor: CastleDB.IconsKind;
-
+    
     public var elements: Array<ui.comp.BaseElementComp>;
     var tmpElements: Array<ui.comp.BaseElementComp>;
+    
+    // Windows
+    var waitOverlay: h2d.Flow;
+    var waitTimeout: Float;
     var windows: Array<ui.win.BaseWindow>;
-
+    
     var canvas(default,null): h2d.Layers;
 
     public var lastKeyCode(default,null): Int;
@@ -31,6 +35,9 @@ class BaseGUI {
     public function new(context:h2d.Layers) {
         elements = [];
         tmpElements = [];
+        waitTimeout = 0;
+        waitOverlay = null;
+
         windows = [];
 
         lastKeyDown = false;
@@ -73,7 +80,7 @@ class BaseGUI {
      * @return h2d.Tile
      */
     public static dynamic function getIcon(id:CastleDB.IconsKind): h2d.Tile {
-        var gfx:Types.TilePos = cast CastleDB.icons.get(id).gfx;
+        var gfx:cdb.Types.TilePos = cast CastleDB.icons.get(id).gfx;
         if (gfx == null) {
             trace('Error: Icon ${id.toString()} not found!');
             return null;
@@ -81,19 +88,12 @@ class BaseGUI {
         return getTile(gfx);
     }
 
-    public static dynamic function getTile(tilePos:Types.TilePos): h2d.Tile {
+    public static dynamic function getTile(tilePos:cdb.Types.TilePos): h2d.Tile {
         if (tilePos == null) {
             return h2d.Tile.fromColor(0xff00ff, 16, 16);
         }
-        if (tilePos.width == null) {
-            tilePos.width = 1;
-        }
-        var w = tilePos.width * tilePos.size;
-        if (tilePos.height == null) {
-            tilePos.height = 1;
-        }
-
-        var h = tilePos.height * tilePos.size;
+        var w = ((tilePos.width == null) ? 1 : tilePos.width) * tilePos.size;
+        var h = ((tilePos.height == null) ? 1 : tilePos.height) * tilePos.size;
         return hxd.Res.load(tilePos.file).toTile().sub(tilePos.x * tilePos.size + 0 * w, tilePos.y * tilePos.size + 0 * h, w, h);
     }
 
@@ -164,7 +164,7 @@ class BaseGUI {
 
     function onKeyDown(ev:hxd.Event) {
         if (ev.keyCode == hxd.Key.ESCAPE) {
-            
+
         }
     }
 
@@ -283,22 +283,256 @@ class BaseGUI {
         event.update(dt);
         ui.comp.BaseElementComp.updateCounter = 0;
 
-        for (i in 0...elements.length) {
-            tmpElements.push(elements[i]);
+        if (isWaiting() || (waitTimeout >= lib.Utils.time())) {
+            waitRelease();
         }
 
+        for (i in 0...elements.length) {
+            tmpElements[i] = elements[i];
+        }
+
+        while (elements.length < tmpElements.length) {
+            tmpElements.pop();
+        }
+        
         for (i in 0...tmpElements.length) {
             var e = tmpElements[i];
             e.updateRec(dt);
         }
 
-        var i = tmpElements.length;
-        while (i-- > 0) {
-            tmpElements.pop();
-        }
-
         BaseGUI.style.sync();
     }
+
+    // #region Windows
+    function isWaiting(): Bool {
+        return false;
+    }
+
+    function attachWindow(w:ui.win.BaseWindow) {}
+
+    function deselect(): Bool {
+        return false;
+    }
+
+    public function displayWindow(window:ui.win.BaseWindow) {
+        if (windows.contains(window)) {
+            return;
+        }
+
+        var i = windows.length;
+        while (i-- > 0) {
+            var w = windows[i];
+            if (window.closeOtherOnOpen(w)) {
+                if (w.closeSelfOnOpen(window)) {
+                    closeWindow(w);
+                }
+            }
+        }
+
+        if (window.parent == null) {
+            attachWindow(window);
+        }
+
+        windows.unshift(window);
+        if (window != null) {
+            if (window.gcOnOpen) {
+                var delay = 0.01;
+                if (Game.inst != null) {
+                    delay = delay + Game.inst.dt;
+                }
+                event.wait(delay, ()->{
+                    #if hl
+                    var gc = haxe.Timer.stamp() - lib.GC.lastGC;
+                    if (gc > lib.GC.cooldown) {
+                        lib.GC.lastGC = Sys.time();
+                        hl.Gc.major();
+                    }
+                    #end
+                });
+            }
+        }
+    }
+
+    public function hasWindowInstance(window:ui.win.BaseWindow): Bool {
+        var i = windows.length;
+        while (i-- > 0) {
+            var w = windows[i];
+            if (w == window) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Remove a specified window `w` from the GUI immediately.
+     * @param w 
+     */
+     public function removeWindow(window:ui.win.BaseWindow) {
+        windows.remove(window);
+        if (window != null) {
+            if (window.parent != null) {
+                window.parent.removeChild(window);
+            }
+        }
+    }
+
+    /**
+     * Close the specified window `window` and remove it from the GUI.
+     * @param window 
+     */
+    public function closeWindow(window:ui.win.BaseWindow) {
+        if (window != null) {
+            window.closeWindow();
+        } else {
+            removeWindow(window);
+        }
+    }
+
+    /**
+     * Clear all windows from the current GUI context.
+     */
+    public function removeAllWindows() {
+        var i = windows.length;
+        while (i-- > 0 ) {
+            var w = windows[i];
+            closeWindow(w);
+        }
+    }
+
+    /**
+     * Removes the window of type `cl`.
+     * @param cl 
+     */
+    public function removeWindowType(cl:Class<ui.win.BaseWindow>): Bool {
+        var window = getWindow(cl);
+        if (window == null) {
+            return false;
+        }
+
+        removeWindow(window);
+        return true;
+    }
+
+    /**
+     * Returns whether `windows` contains a `BaseWindow` class instance with optional `filter` criteria.
+     * @param cl 
+     * @param filter 
+     * @return Bool
+     */
+    public function hasWindow(cl:Class<ui.win.BaseWindow>, ?filter:ui.comp.DynamicElementComp->Bool): Bool {
+        for (i in 0...windows.length) {
+            var w = windows[i];
+            if (Std.isOfType(w, cl)) {
+                if ((filter == null) || filter(w)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns the most recent occurance of `cl`, with optional `filter` criteria.
+     * @param cl 
+     * @param filter 
+     * @return ui.comp.DynamicElement
+     */
+    public function getWindow(cl:Class<ui.win.BaseWindow>, ?filter:ui.comp.DynamicElementComp->Bool): ui.win.BaseWindow {
+        var i = windows.length;
+        while (i-- > 0) {
+            var w = windows[i];
+            if (Std.isOfType(w, cl)) {
+                if ((filter == null) || filter(w)) {
+                    return w;
+                }
+            }
+        }
+        return null;
+    }
+
+    public function toggleWindow(cl:Class<ui.win.BaseWindow>, ?filter:ui.comp.DynamicElementComp->Bool): ui.win.BaseWindow {
+        var w = getWindow(cl, filter);
+        if (w != null) {
+            removeWindow(w);
+            return null;
+        }
+        w = Type.createInstance(cl, []);
+        displayWindow(w);
+        return w;
+    }
+
+    /**
+     * Check if the GUI has any blocking windows
+     */
+    public function hasBlockingWindow() {
+        for (i in 0...windows.length) {
+            var window = windows[i];
+            if (Std.isOfType(window, ui.win.BaseWindow)) {
+                var w:ui.win.BaseWindow = cast window;
+                if (w.blockInputs) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function closeFirstClosableUI(fromClick:Bool=true): Bool {
+        if (hasLockedTooltip()) {
+            cleanTooltips();
+        }
+
+        var i = windows.length;
+        while (i-- > 0) {
+            var w = windows[i];
+            if (w != null) {
+                if (!w.ignoreAutoClose) {
+                    if (w.closeWindow()) {
+                        return true;
+                    }
+                }
+            }
+            removeWindow(w);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns true if any item in `windows` satisfies the provided function `fn`.
+     * @param fn 
+     * @return Bool
+     */
+    public function anyWindow(fn:ui.win.BaseWindow->Bool): Bool {
+        for (i in 0...windows.length) {
+            var w = windows[i];
+            if ((w != null) && fn(w)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns true if any open window is unable to close.
+     */
+    public function anyForcedOpenWindow(): Bool {
+        return anyWindow((w)->{
+            return !w.canClose();
+        });
+    }
+
+    /**
+     * Returns true if any window is blocking user inputs.
+     */
+    public function anyBlockingWindow(): Bool {
+        return anyWindow((w)->{
+            return w.blockInputs;
+        });
+    }
+
+    // #endregion
 
     public static function loadFontFace(base:h2d.Font, ?variant:String): h2d.Font {
         var fontName = switch(variant) {
@@ -324,7 +558,7 @@ class BaseGUI {
 
         return h2d.Tile.fromColor(0xff00ff, 8, 8);
     }
-
+    
 }
 
 class RootContainer extends h2d.Flow implements h2d.domkit.Object {
